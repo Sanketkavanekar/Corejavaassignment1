84a23573be834227cb5fd23f95d4b3fcc13a1315
Sanketkavanekar <sanketkavanekar1125@gmail.com>
2025-03-06 01:17:20 +0530

    Add files via upload

---
+++ exam5.java
@@ -0,0 +1,18 @@
+class exam5{
+    public static void main(string [] args){
+		 char A = 'A';  // Single character
+        char B = 'B';
+        char Digit = '5'; 
+        char Sym = '@';  
+        
+        
+        String message = "Hello, Java!";
+        
+        
+        
+        System.out.println( charA + " " + (int) charA);
+        System.out.println( charB + " " + (int) charB);
+        System.out.println( charDigit + " " + (int) charDigit);
+        System.out.println(charSymbol + " " + (int) charSymbol);
+	}
+}
---
+++ exam6.java
@@ -0,0 +1,18 @@
+class exam6{
+    public static void main(String [] args){
+		 char A = 'A';  // Single character
+        char B = 'B';
+        char Digit = '5'; 
+        char Sym = '@';  
+        
+        
+        
+        
+        
+        
+        System.out.println( A + " " + (int) A);
+        System.out.println( B + " " + (int) B);
+        System.out.println( Digit + " " + (int) Digit);
+        System.out.println(Sym + " " + (int) Sym);
+	}
+}
